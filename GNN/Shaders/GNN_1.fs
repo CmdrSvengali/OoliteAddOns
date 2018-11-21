9a654884e0d0f60d2a1f6f6286f5a9687f00c380
CmdrSvengali <krienke.oliver@web.de>
2018-11-21 16:24:34 +0100

    Simplified GNN shader.

--- GNN/Shaders/GNN_1.fs
+++ GNN/Shaders/GNN_1.fs
@@ -1,17 +1,11 @@
-// GNN, 9 ALU
-uniform sampler2D colorMap,fxMap;
-uniform float Time;
+// GNN, 7 ALU
 varying vec2 vTexCoord;
+uniform sampler2D colorMap;
+uniform float Time;
 
 void main()
 {
-	vec2 dir = vec2(0.25);
-	vec3 endColor = texture2D(colorMap, vTexCoord).rgb;
-	vec3 sum = endColor;
-	for (int i = 0; i < 3; i++)
-	{
-		sum += texture2D(fxMap, dir * Time).rgb;
-	}
-	sum *= 0.25*clamp(sin(3.14*(vTexCoord.x-Time)),0.0,1.0);
-	gl_FragColor = vec4(mix(endColor,sum,endColor.g),1.0);
+	vec3 color = texture2D(colorMap, vTexCoord).rgb;
+	float sum = max(sin(vTexCoord.x-Time),0.0);
+	gl_FragColor = vec4(color+color*sum,1.0);
 }
